100c1ae0ee5342d0def0daf6a3f994cf0eefdd3c
vradionovskii <82278782+vradionovskii@users.noreply.github.com>
2023-09-06 11:43:10 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -41,7 +41,7 @@ function App({ Component, pageProps }) {
         ></iframe>
       </noscript>
       <SmartOutline />
-
+      <Metrika />
       <SEO
         description={pageProps.data.description}
         title={pageProps.data.title}
@@ -57,6 +57,41 @@ function App({ Component, pageProps }) {
       <Component {...pageProps} />
       <Footer data={pageProps.footer} currentPage={router.route} />
     </ProgressiveImageSupportProvider>
+  );
+}
+
+function Metrika() {
+  return (
+    <>
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/93488336"
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
+
+      <Script
+        id="metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(93488336, "init", {
+     clickmap:true,
+     trackLinks:true,
+     accurateTrackBounce:true,
+     webvisor:true
+    });`,
+        }}
+      />
+    </>
   );
 }
 
