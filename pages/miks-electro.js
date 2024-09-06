dafb866c931bd25268172b01cecc7d0b13c6185a
miks-kart <118816369+miks-kart@users.noreply.github.com>
2024-09-06 22:03:51 +0300

    Update miks-electro.js

--- pages/miks-electro.js
+++ pages/miks-electro.js
@@ -130,6 +130,48 @@ export default function Index({ data, gallery, gallery2, hero }) {
           </Link>
         </BackgroundImage>
       </section>
+      <section className="w-screen min-h-screen md:bg-[#F7F7F7]">
+        <div className="page-container md:!pt-16 !pb-16">
+          <article>
+            <h1 className="md:!pb-10 theme-heading">{data.headingOne}</h1>
+            <iframe
+              src="https://yandex.com/map-widget/v1/-/CDwdQXJp"
+              frameBorder="1"
+              allowFullScreen={true}
+              className="grayscale w-full aspect-square md:aspect-[3.32]"
+              title="map"
+            ></iframe>
+            <div className="justify-between pt-8 space-y-5 md:space-y-0 md:flex">
+              {data.points.map(({ point }, i) => (
+                <div className="" key={point.icon}>
+                  <img
+                    src={point.icon}
+                    alt={point.text}
+                    className="w-5 h-5 mb-[0.625rem]"
+                  />
+                  <p className="theme-text">{point.heading}</p>
+                  {i !== 2 ? (
+                    <a
+                      href={
+                        point.text.includes("@")
+                          ? `mailto:${point.text}`
+                          : `tel:${point.text}`
+                      }
+                      className="text-xl font-bold md:text-2xl"
+                    >
+                      {point.text}
+                    </a>
+                  ) : (
+                    <p className="text-xl font-bold md:text-2xl">
+                      {point.text}
+                    </p>
+                  )}
+                </div>
+              ))}
+            </div>
+          </article>
+        </div>
+      </section>
     </AnchorSmoothScroll>
   );
 }
