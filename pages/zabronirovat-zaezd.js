6ab43e84a2a534b662cb97ea6f5a729f5d031678
miks-kart <118816369+miks-kart@users.noreply.github.com>
2025-03-27 22:18:23 +0300

    Update zabronirovat-zaezd.js

--- pages/zabronirovat-zaezd.js
+++ pages/zabronirovat-zaezd.js
@@ -26,15 +26,15 @@ export default function Index({ hero, heroTwo }) {
         <div className="page-container md:!pt-16 md:!pb-9 !pb-5 wide">
           <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
             <div className="flex grey-card direction-column bg-grey justify-space-between">
-              <p className="text-big">Забронируйте заезд по номеру телефона</p>
-              <a className="theme-button button-rounded" href="tel:+74956403302">+7 (495) 640 33 02</a>
+              <p className="text-big mb-14px md:mb-0">Забронируйте заезд по номеру телефона</p>
+              <a className="theme-button button-rounded mb-14px md:mb-0" href="tel:+74956403302">+7 (495) 640 33 02</a>
               <div>
                 <p className="text-small mb-14px">Часы работы: ежедневно (10:00-18:00)</p>
                 <p className="text-small">За исключением дней определенных администрацией для подготовки трассы или иных работ.</p>
               </div>
             </div>
             <div className="flex grey-card direction-column bg-grey justify-space-between">
-              <p className="text-big">Приезжайте на трассу за 20 минут до начала заезда</p>
+              <p className="text-big mb-14px md:mb-0">Приезжайте на трассу за 20 минут до начала заезда</p>
               <p className="text-small mb-14px">Приезжайте по адресу: <span className="text-red d-block">г. Рязань, пос. Секиотово <br />Комплекс расположен в 15 минутах от центра Рязани по Михайловскому шоссе. <br />Поворот с трассы М5 на г. Тула.</span></p>
               <a className="theme-button button-rounded mb-14px" href="tel:+74956403302">Проложить маршрут на Яндекс Картах</a>
               <a className="theme-button button-rounded" href="tel:+74956403302">Проложить маршрут в Яндекс Навигаторе</a>
@@ -71,7 +71,7 @@ export default function Index({ hero, heroTwo }) {
           </div>
         </div>
       </section>
-      <section className="w-screen md:bg-white">
+      <section className="w-screen bg-white">
         <div className="page-container wide !space-y-0 !py-0 text-center">
           <h2 className="big-header">
             САМАЯ ПРОТЯЖЕННАЯ
@@ -84,19 +84,19 @@ export default function Index({ hero, heroTwo }) {
       <section className="w-screen bg-white">
         <div className="page-container md:!pt-16 md:!pb-9 !pb-5 wide">
           <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-            <div className="flex dark-grey-card direction-column md:direction-row bg-dark-grey align-items-center">
+            <div className="flex dark-grey-card bg-dark-grey align-items-center">
               <img src="/images/icon-bron-1.png" alt="icon" />
               <p>В стоимость заезда включено предоставление прокатного шлема и одноразового подшлемника</p>
             </div>
-            <div className="flex dark-grey-card direction-column md:direction-row bg-dark-grey align-items-center">
+            <div className="flex dark-grey-card bg-dark-grey align-items-center">
               <img src="/images/icon-bron-2.png" alt="icon" />
               <p>В летний период запрещены заезды в открытой обуви (сандали, босоножки и др.), в обуви на каблуке и с длинным (не убранными) волосами </p>
             </div>
-            <div className="flex dark-grey-card direction-column md:direction-row bg-dark-grey align-items-center">
+            <div className="flex dark-grey-card bg-dark-grey align-items-center">
               <img src="/images/icon-bron-3.png" alt="icon" />
               <p>Минимальный рост для детей в одноместном карте составляет 150см. Минимальный возраст для заезда на 2х-местном карте пассажиром — 6 лет</p>
             </div>
-            <div className="flex dark-grey-card direction-column md:direction-row bg-dark-grey align-items-center">
+            <div className="flex dark-grey-card bg-dark-grey align-items-center">
               <img src="/images/icon-bron-4.png" alt="icon" />
               <p>Запись на заезды осуществляется только по номеру телефону ресепшен комплекса </p>
             </div>
