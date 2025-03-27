f70340dec6f40739fb51efe8250f18a3d022631c
miks-kart <118816369+miks-kart@users.noreply.github.com>
2025-03-27 17:49:38 +0300

    Update zabronirovat-zaezd.js

--- pages/zabronirovat-zaezd.js
+++ pages/zabronirovat-zaezd.js
@@ -22,21 +22,21 @@ export default function Index({ gallery }) {
             </div>
             <div className="flex grey-card direction-column bg-grey justify-space-between">
               <p className="text-big">Приезжайте на трассу за 20 минут до начала заезда</p>
-              <p className="text-small">Приезжайте по адресу: <span className="text-red">г. Рязань, пос. Секиотово <br />Комплекс расположен в 15 минутах от центра Рязани по Михайловскому шоссе.Поворот с трассы М5 на г. Тула.</span></p>
+              <p className="text-small">Приезжайте по адресу: <span className="text-red">г. Рязань, пос. Секиотово <br />Комплекс расположен в 15 минутах от центра Рязани по Михайловскому шоссе. <br />Поворот с трассы М5 на г. Тула.</span></p>
               <a className="theme-button" href="tel:+74956403302">Проложить маршрут на Яндекс Картах</a>
               <a className="theme-button" href="tel:+74956403302">Проложить маршрут в Яндекс Навигаторе</a>
             </div>
             <div className="flex grey-card direction-column bg-grey justify-space-between">
               <p className="text-big">Получите экипировку</p>
               <p className="text-big">Пройдите инструктаж</p>
-              <p className="text-red">Прыгайте в карт и гоняйте!</p>
+              <p className="text-big text-red">Прыгайте в карт и гоняйте!</p>
             </div>
           </div>
         </div>
       </section>
       <section className="w-screen bg-white">
         <div className="page-container md:!pt-16 md:!pb-9 !pb-5 wide">
-          <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+          <div className="flex direction-column md:direction-row">
             <div className="cart-card">
               <img src="/images/cart-bron.png" alt="Miks Kart SPORT 9л.с." />
               <p>Miks Kart SPORT 9л.с.</p>
@@ -59,7 +59,7 @@ export default function Index({ gallery }) {
         </div>
       </section>
       <section className="w-screen md:bg-white">
-        <div className="page-container wide !space-y-0 !py-0">
+        <div className="page-container wide !space-y-0 !py-0 text-center">
           <h2 className="big-header">
             САМАЯ ПРОТЯЖЕННАЯ
             <span>ОТКРЫТАЯ ТРАССА ДЛЯ КАРТИНГА В РОССИИ!</span>
@@ -71,29 +71,29 @@ export default function Index({ gallery }) {
       <section className="w-screen bg-white">
         <div className="page-container md:!pt-16 md:!pb-9 !pb-5 wide">
           <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-            <div className="flex dark-grey-card direction-column md:direction-row bg-grey align-items-center">
+            <div className="flex dark-grey-card direction-column md:direction-row bg-dark-grey align-items-center">
               <img src="/images/icon-bron-1.png" alt="icon" />
               <p>В стоимость заезда включено предоставление прокатного шлема и одноразового подшлемника</p>
             </div>
-            <div className="flex dark-grey-card direction-column md:direction-row bg-grey align-items-center">
+            <div className="flex dark-grey-card direction-column md:direction-row bg-dark-grey align-items-center">
               <img src="/images/icon-bron-2.png" alt="icon" />
-              <p>В летний период запрещены заезды в открытой обуви( сандали, босоножки и др.), в обуви на каблуке и с длинным (не убранными) волосами </p>
+              <p>В летний период запрещены заезды в открытой обуви (сандали, босоножки и др.), в обуви на каблуке и с длинным (не убранными) волосами </p>
             </div>
-            <div className="flex dark-grey-card direction-column md:direction-row bg-grey align-items-center">
+            <div className="flex dark-grey-card direction-column md:direction-row bg-dark-grey align-items-center">
               <img src="/images/icon-bron-3.png" alt="icon" />
               <p>Минимальный рост для детей в одноместном карте составляет 150см. Минимальный возраст для заезда на 2х-местном карте пассажиром — 6 лет</p>
             </div>
-            <div className="flex dark-grey-card direction-column md:direction-row bg-grey align-items-center">
+            <div className="flex dark-grey-card direction-column md:direction-row bg-dark-grey align-items-center">
               <img src="/images/icon-bron-4.png" alt="icon" />
               <p>Запись на заезды осуществляется только по номеру телефону ресепшен комплекса </p>
             </div>
           </div>
         </div>
       </section>
-      <section>
+      <section className="w-screen bg-white">
         <div className="page-container md:!pt-16 md:!pb-9 !pb-5 wide text-center">
           <p>ЗАБРОНИРУЙТЕ ЗАЕЗД ПО ТЕЛЕФОНУ:</p>
-          <a className="theme-button" href="tel:+7 (495) 640 33 02">+7 (495) 640 33 02</a>
+          <a className="theme-button" href="tel:+74956403302">+7 (495) 640 33 02</a>
         </div>
       </section>
     </AnchorSmoothScroll>
