5f7b35849bf86377a69d68f648dd93837b8e03eb
miks-kart <118816369+miks-kart@users.noreply.github.com>
2025-05-19 17:59:35 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,6 @@ export default function Index({ data, postOne, postTwo, contactForm, hero }) {
             >
               <span className="relative">{data.testdrive.text}</span>
             </Link>
-                <div><a href="https://miks-electrokarting.ru/"><img src="https://miks-electrokarting.ru/local/templates/miks-electrokarting_pages/img/miks-logo.svg"></a></div>
           </BackgroundImage>
         </section>
         <div className="page-container !pt-10 md:!pt-16 !pb-4">
