1acdf2d24b53bb2c7c196282abfcdf1e644325a5
miks-kart <118816369+miks-kart@users.noreply.github.com>
2025-07-22 17:52:49 +0300

    Update index.js

--- pages/prokatnye-karty/index.js
+++ pages/prokatnye-karty/index.js
@@ -3,26 +3,35 @@ import { getFluidImage } from "@components/image/imageFunctions";
 import { useEffect, useState } from "react";
 import Image from "@components/image/Image";
 
-export default function Index({ data, heroOne, heroTwo, heroThree }) {
-  const [activeSlide, setActiveSlide] = useState(0); // 0 - первый, 1 - второй, 2 - третий
-  const [hoveredSlide, setHoveredSlide] = useState(null);
-
+export default function Index({ data, heroOne, heroTwo }) {
+  const [isFirst, setIsFirst] = useState(true);
+  const [first, setFirst] = useState(false);
+  const [second, setSecond] = useState(false);
   useEffect(() => {
     let timer;
-    if (hoveredSlide === null) {
+    if (!first && !second) {
       timer = setInterval(() => {
-        setActiveSlide((prev) => (prev + 1) % 3); // Циклически переключаем между 0, 1, 2
+        setIsFirst((isFirst) => !isFirst);
       }, 5000);
+    }
+
+    if (first) {
+      setIsFirst(true);
+      clearInterval(timer);
+    }
+    if (second) {
+      setIsFirst(false);
+      clearInterval(timer);
     }
 
     return () => {
       clearInterval(timer);
     };
-  }, [hoveredSlide]);
-
+  }, [first, second]);
   return (
     <section className="w-screen bg-white">
       <div className="relative flex items-end justify-start object-cover w-full h-screen">
+       
         <Image
           preload
           loading="eager"
@@ -35,18 +44,9 @@ export default function Index({ data, heroOne, heroTwo, heroThree }) {
           src={heroTwo}
           alt=""
           className={`${
-            activeSlide !== 1 ? "opacity-0" : "opacity-100"
+            isFirst ? "opacity-0" : "opacity-100"
           } !absolute inset-0 w-full h-full transition-opacity duration-500`}
         />
-        <Image
-          loading="eager"
-          src={heroThree}
-          alt=""
-          className={`${
-            activeSlide !== 2 ? "opacity-0" : "opacity-100"
-          } !absolute inset-0 w-full h-full transition-opacity duration-500`}
-        />
-        
         <div
           style={{
             background:
@@ -55,25 +55,22 @@ export default function Index({ data, heroOne, heroTwo, heroThree }) {
           className="absolute inset-0 z-30"
         ></div>
         <div className="page-container relative z-40 wide md:!py-16 !py-5">
-          <h1 className="prokatnye-karty">Прокатные карты</h1>
-          
-          {/* Первый карт */}
+             <h1 className="prokatnye-karty">Прокатные карты</h1>
           <Link
             href={data.carts[0].cart.link}
-            onFocus={() => setHoveredSlide(0)}
-            onBlur={() => setHoveredSlide(null)}
-            onMouseOver={() => setHoveredSlide(0)}
-            onMouseLeave={() => setHoveredSlide(null)}
+            onFocus={() => setFirst(true)}
+            onBlur={() => setFirst(false)}
+            onMouseOver={() => setFirst(true)}
+            onMouseLeave={() => setFirst(false)}
             className="block pb-2 md:pb-5"
-            onClick={() => setActiveSlide(0)}
           >
             <p
               className={`${
-                hoveredSlide === null
+                !first && !second
                   ? "underline underline-offset-8 decoration-4"
                   : ""
               } ${
-                hoveredSlide === 0 || activeSlide === 0
+                isFirst && first
                   ? "underline decoration-primary-red underline-offset-8 decoration-4"
                   : ""
               } font-black inline-flex text-3xl md:text-6xl italic uppercase text-[#F6F6F6]`}
@@ -81,7 +78,7 @@ export default function Index({ data, heroOne, heroTwo, heroThree }) {
               {data.carts[0].cart.heading}
               <span
                 className={`${
-                  hoveredSlide === 0 || activeSlide === 0 ? "!opacity-100" : ""
+                  isFirst && first ? "!opacity-100" : ""
                 } mt-1 md:ml-6 ml-3 duration-150 opacity-0`}
               >
                 <img
@@ -93,81 +90,35 @@ export default function Index({ data, heroOne, heroTwo, heroThree }) {
             </p>
             <p
               className={`${
-                activeSlide === 0 ? "!opacity-100" : ""
+                isFirst ? "!opacity-100" : ""
               } md:text-xl font-light text-white duration-150 opacity-0`}
             >
               {data.carts[0].cart.text}
             </p>
           </Link>
-          
-          {/* Второй карт */}
           <Link
             href={data.carts[1].cart.link}
-            onFocus={() => setHoveredSlide(1)}
-            onBlur={() => setHoveredSlide(null)}
-            onMouseOver={() => setHoveredSlide(1)}
-            onMouseLeave={() => setHoveredSlide(null)}
-            className="pb-2 md:pb-5"
-            onClick={() => setActiveSlide(1)}
-          >
-            <p
-              className={`${
-                hoveredSlide === null
-                  ? "underline underline-offset-8 decoration-4"
-                  : ""
-              } ${
-                hoveredSlide === 1 || activeSlide === 1
-                  ? "underline decoration-primary-red underline-offset-8 decoration-4"
-                  : ""
-              } font-black inline-flex text-3xl md:text-6xl italic uppercase text-[#F6F6F6]`}
-            >
-              {data.carts[1].cart.heading}
-              <span
-                className={`${
-                  hoveredSlide === 1 || activeSlide === 1 ? "!opacity-100" : ""
-                } mt-1 md:ml-6 ml-3 duration-150 opacity-0`}
-              >
-                <img
-                  src="/images/check-up.svg"
-                  alt="check up"
-                  className="w-4 h-4 md:w-5 md:h-5"
-                />
-              </span>
-            </p>
-            <p
-              className={`${
-                activeSlide === 1 ? "!opacity-100" : ""
-              } md:text-xl font-light text-white duration-150 opacity-0`}
-            >
-              {data.carts[1].cart.text}
-            </p>
-          </Link>
-          
-          {/* Третий карт */}
-          <Link
-            href={data.carts[2].cart.link}
-            onFocus={() => setHoveredSlide(2)}
-            onBlur={() => setHoveredSlide(null)}
-            onMouseOver={() => setHoveredSlide(2)}
-            onMouseLeave={() => setHoveredSlide(null)}
+            onFocus={() => setSecond(true)}
+            onBlur={() => setSecond(false)}
+            onMouseOver={() => setSecond(true)}
+            onMouseLeave={() => setSecond(false)}
             className="group"
-            onClick={() => setActiveSlide(2)}
           >
             <p
               className={`${
-                hoveredSlide === null
+                !first && !second
                   ? "underline underline-offset-8 decoration-4"
                   : ""
               } ${
-                hoveredSlide === 2 || activeSlide === 2
+                !isFirst && second
                   ? "underline decoration-primary-red underline-offset-8 decoration-4"
                   : ""
               } font-black inline-flex text-3xl md:text-6xl italic uppercase text-[#F6F6F6]`}
             >
-              <span>{data.carts[2].cart.heading}</span>
+              <span>{data.carts[1].cart.heading}</span>
               <span
                 className={`${
-                  hoveredSlide === 2 || activeSlide === 2 ? "!opacity-100" : ""
+                  !isFirst && second ? "!opacity-100" : ""
                 } mt-1 md:ml-6 ml-2 duration-150 opacity-0`}
               >
                 <img
@@ -179,15 +130,40 @@ export default function Index({ data, heroOne, heroTwo, heroThree }) {
             </p>
             <p
               className={`${
-                activeSlide === 2 ? "!opacity-100" : ""
+                !isFirst ? "!opacity-100" : ""
               } md:text-xl font-light text-white duration-150 opacity-0`}
             >
-              {data.carts[2].cart.text}
+              {data.carts[1].cart.text}
             </p>
           </Link>
         </div>
       </div>
-      {/* Остальной код остается без изменений */}
+      <section className="bg-[#f7f7f7]">
+        <div className="md:flex justify-between items-center  page-container !space-y-0 !py-7 wide">
+          <div className="flex items-center">
+            <img
+              style={{
+                filter: "drop-shadow(0px 50px 100px rgba(0, 0, 0, 0.25))",
+                transform: "translateZ(0)",
+              }}
+              src={data.imageOne}
+              alt="socket"
+              className="w-1/2 pr-4 md:pr-0 md:mr-10 md:w-28"
+            />
+            <div className="">
+              <p className="pb-3 text-2xl italic font-black uppercase text-primary-dark">
+                {data.headingOne}
+              </p>
+              <p className="max-w-sm theme-text">{data.textOne}</p>
+            </div>
+          </div>
+          <div className="table pt-8 mx-auto md:pt-0 md:mx-0">
+            <Link href={data.buttonOne.link} className="theme-button">
+              <span className="relative">{data.buttonOne.text}</span>
+            </Link>
+          </div>
+        </div>
+      </section>
     </section>
   );
 }
@@ -201,15 +177,15 @@ export async function getStaticProps() {
 
   const heroOne = await getFluidImage(
     content.default.attributes.carts[0].cart.image,
-    { webp: true }
+    {
+      webp: true,
+    }
   );
   const heroTwo = await getFluidImage(
     content.default.attributes.carts[1].cart.image,
-    { webp: true }
-  );
-  const heroThree = await getFluidImage(
-    content.default.attributes.carts[2].cart.image,
-    { webp: true }
+    {
+      webp: true,
+    }
   );
 
   return {
@@ -220,7 +196,6 @@ export async function getStaticProps() {
       seo: seo.default.attributes,
       heroOne,
       heroTwo,
-      heroThree,
     },
   };
 }
